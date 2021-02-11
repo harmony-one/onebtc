949acdc2db06fa26c6287f9cac98794b6d7decb4
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2021-02-10 22:00:44 -0800

    [testing] Make build.test.ts more simple

--- contract/relay/test/build.test.ts
+++ contract/relay/test/build.test.ts
@@ -18,41 +18,27 @@ describe("Build", () => {
     let block = genesis();
     let contract : Contract
 
-    describe("genesis", () => {
-      it("should deploy and init", async () => {
-          block = genesis();
-          contract = await DeployTestRelay(HarmonyDeployWallet, {
-              header: '0x' + block.toHex(true),
-              height: 1,
-          });
-      })
+    it("relay should deploy with genesis init", async () => {
+        block = genesis();
+        contract = await DeployTestRelay(HarmonyDeployWallet, {
+            header: '0x' + block.toHex(true),
+            height: 1,
+        });
     })
-
-    describe("block 1", () => {
-        it("should submit", async () => {
-            block = generate("bcrt1qu96jmjrfgpdynvqvljgszzm9vtzp7czquzcu6q", block.getHash());
-            contract.submitBlockHeader('0x' + block.toHex(true))
-        })
+    it("block 1 should submit", async () => {
+        block = generate("bcrt1qu96jmjrfgpdynvqvljgszzm9vtzp7czquzcu6q", block.getHash());
+        contract.submitBlockHeader('0x' + block.toHex(true))
     })
-
-    describe("best block", () => {
-        it("should be block 1 hash", async () => {
-            let best = await contract.getBestBlock();
-            expect(best.digest).to.eq("0x9588627a4b509674b5ed7180cb2f9c8679fe5f1c8a6378069af0f2b8c2ff831f");
-        })
+    it("best block should be block 1 hash", async () => {
+        let best = await contract.getBestBlock();
+        expect(best.digest).to.eq("0x9588627a4b509674b5ed7180cb2f9c8679fe5f1c8a6378069af0f2b8c2ff831f");
     })
-
-    describe("block 2", () => {
-        it("should submit", async () => {
-            block = generate("bcrt1qu96jmjrfgpdynvqvljgszzm9vtzp7czquzcu6q", block.getHash());
-            contract.submitBlockHeader('0x' + block.toHex(true))
-        })
+    it("block 2 should submit", async () => {
+        block = generate("bcrt1qu96jmjrfgpdynvqvljgszzm9vtzp7czquzcu6q", block.getHash());
+        contract.submitBlockHeader('0x' + block.toHex(true))
     })
-
-    describe("best block", () => {
-        it("should be block 2 hash", async () => {
-            let best = await contract.getBestBlock();
-            expect(best.digest).to.eq("0x7b02735fdcd34c70e65d1442949bd0a0fae69aedabfc05503f3ae5998a8f4348");
-        })
+    it("best block should be block 2 hash", async () => {
+        let best = await contract.getBestBlock();
+        expect(best.digest).to.eq("0x7b02735fdcd34c70e65d1442949bd0a0fae69aedabfc05503f3ae5998a8f4348");
     })
 });
