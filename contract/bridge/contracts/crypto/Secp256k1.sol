c64ad84f7cdc859b57071a0eb08eea66adf130bc
peekpi <894646171@qq.com>
2021-05-21 12:28:20 +0800

    ecc curve library from https://github.com/witnet/elliptic-curve-solidity

---
+++ contract/bridge/contracts/crypto/Secp256k1.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "./EllipticCurve.sol";
+
+/**
+ * @title Secp256k1 Elliptic Curve
+ * @notice Example of particularization of Elliptic Curve for secp256k1 curve
+ * @author Witnet Foundation
+ */
+library Secp256k1 {
+    uint256 constant GX =
+        0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798;
+    uint256 constant GY =
+        0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8;
+    uint256 constant AA = 0;
+    uint256 constant BB = 7;
+    uint256 constant PP =
+        0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F;
+
+    function ecMul(
+        uint256 scale,
+        uint256 Px,
+        uint256 Py
+    ) internal pure returns (uint256, uint256) {
+        return EllipticCurve.ecMul(scale, Px, Py, AA, PP);
+    }
+
+    /*
+    function priToAddress(uint256 pri) internal view returns(address) {
+        (uint256 px, uint256 py) = ecMul(pri, GX, GY);
+        return btcAddress(compression(px, py));
+    }
+    */
+    // covert uncompression public key(64bytes) to compression publick key(33bytes)
+    function compression(uint256 x, uint256 y)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        uint8 parity = 2 + uint8(y & 1);
+        return abi.encodePacked(parity, x);
+    }
+}
