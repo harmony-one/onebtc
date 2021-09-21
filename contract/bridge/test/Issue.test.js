7ddcaf6ccbaf9291c4d048a4a9a3f460396cd536
apptreeso <superwebdev19@gmail.com>
2021-09-22 02:27:10 +1000

    add initial test

---
+++ contract/bridge/test/Issue.test.js
@@ -0,0 +1,57 @@
+const BN = require("bn.js");
+
+const OneBtc = artifacts.require("OneBtc");
+const RelayMock = artifacts.require("RelayMock");
+const { issueTxMock } = require("./mock/btcTxMock");
+
+const bitcoin = require("bitcoinjs-lib");
+const bn = (b) => BigInt(`0x${b.toString("hex")}`);
+
+contract("Issue unit test", (accounts) => {
+  before(async function () {
+    const IRelay = await RelayMock.new();
+    this.OneBtc = await OneBtc.new(IRelay.address);
+
+    this.vaultId = accounts[1];
+    this.issueRequester = accounts[2];
+
+    this.OneBtcBalance = 0;
+    this.OneBtcBalanceVault = 0;
+  });
+
+  it("Issue 1 BTC", async function () {
+    const IssueAmount = 1 * 1e8;
+    const IssueReq = await this.OneBtc.requestIssue(IssueAmount, this.vaultId, {
+      from: this.issueRequester,
+      value: IssueAmount,
+    });
+    const IssueEvent = IssueReq.logs.filter(
+      (log) => log.event == "IssueRequest"
+    )[0];
+    const issueId = IssueEvent.args.issueId;
+    const btcAddress = IssueEvent.args.btcAddress;
+    const btcBase58 = bitcoin.address.toBase58Check(
+      Buffer.from(btcAddress.slice(2), "hex"),
+      0
+    );
+    const btcTx = issueTxMock(issueId, btcBase58, IssueAmount);
+    const btcBlockNumberMock = 1000;
+    const btcTxIndexMock = 2;
+    const heightAndIndex = (btcBlockNumberMock << 32) | btcTxIndexMock;
+    const headerMock = Buffer.alloc(0);
+    const proofMock = Buffer.alloc(0);
+    await this.OneBtc.executeIssue(
+      this.issueRequester,
+      issueId,
+      proofMock,
+      btcTx.toBuffer(),
+      heightAndIndex,
+      headerMock
+    );
+
+    this.OneBtcBalance = await this.OneBtc.balanceOf(this.issueRequester);
+    this.OneBtcBalanceVault = await this.OneBtc.balanceOf(this.vaultId);
+    assert.equal(this.OneBtcBalance.toString(), IssueEvent.args.amount.toString());
+    assert.equal(this.OneBtcBalanceVault.toString(), IssueEvent.args.fee.toString());
+  });
+});
