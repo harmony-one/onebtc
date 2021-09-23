be7930bc542075f7e62f0bc0ca8ec75fdb59492a
apptreeso <superwebdev19@gmail.com>
2021-09-23 11:08:13 +1000

    add the test for slash and issue revert

--- contract/bridge/test/Issue.test.js
+++ contract/bridge/test/Issue.test.js
@@ -54,4 +54,81 @@ contract("Issue unit test", (accounts) => {
     assert.equal(this.OneBtcBalance.toString(), IssueEvent.args.amount.toString());
     assert.equal(this.OneBtcBalanceVault.toString(), IssueEvent.args.fee.toString());
   });
+
+  it("Error on requester is not a executor of issue call", async function () {
+    const IssueAmount = 1 * 1e8;
+    const IssueReq = await this.OneBtc.requestIssue(IssueAmount, this.vaultId, {
+      from: this.issueRequester,
+      value: IssueAmount,
+    });
+    const IssueEvent = IssueReq.logs.filter(
+      (log) => log.event == "IssueRequest"
+    )[0];
+    const issueId = IssueEvent.args.issueId;
+    const btcAddress = IssueEvent.args.btcAddress;
+    const btcBase58 = bitcoin.address.toBase58Check(
+      Buffer.from(btcAddress.slice(2), "hex"),
+      0
+    );
+    const btcTx = issueTxMock(issueId, btcBase58, IssueAmount / 4);
+    const btcBlockNumberMock = 1000;
+    const btcTxIndexMock = 2;
+    const heightAndIndex = (btcBlockNumberMock << 32) | btcTxIndexMock;
+    const headerMock = Buffer.alloc(0);
+    const proofMock = Buffer.alloc(0);
+
+    await expectRevert(this.OneBtc.executeIssue(
+      this.issueRequester,
+      issueId,
+      proofMock,
+      btcTx.toBuffer(),
+      heightAndIndex,
+      headerMock
+    ), 'InvalidExecutor');
+  });
+  
+  it("Slash in case the transferred BTC amount is smaller than the requested OneBTC amount", async function () {
+    const IssueAmount = 1 * 1e8;
+    const IssueReq = await this.OneBtc.requestIssue(IssueAmount, this.vaultId, {
+      from: this.issueRequester,
+      value: IssueAmount,
+    });
+    const IssueEvent = IssueReq.logs.filter(
+      (log) => log.event == "IssueRequest"
+    )[0];
+    const issueId = IssueEvent.args.issueId;
+    const btcAddress = IssueEvent.args.btcAddress;
+    const btcBase58 = bitcoin.address.toBase58Check(
+      Buffer.from(btcAddress.slice(2), "hex"),
+      0
+    );
+    const btcTx = issueTxMock(issueId, btcBase58, IssueAmount / 4);
+    const btcBlockNumberMock = 1000;
+    const btcTxIndexMock = 2;
+    const heightAndIndex = (btcBlockNumberMock << 32) | btcTxIndexMock;
+    const headerMock = Buffer.alloc(0);
+    const proofMock = Buffer.alloc(0);
+    const ExecuteReq = await this.OneBtc.executeIssue(
+      this.issueRequester,
+      issueId,
+      proofMock,
+      btcTx.toBuffer(),
+      heightAndIndex,
+      headerMock,
+      {
+        from: this.issueRequester
+      }
+    );
+    const ExecuteEvent = ExecuteReq.logs.filter(
+      (log) => log.event == "SlashCollateral"
+    )[0];
+    assert.equal(ExecuteEvent.args.amount, IssueAmount - (IssueAmount / 4));
+
+    const beforeOneBtcBalance = this.OneBtcBalance;
+    const beforeOneBtcBalanceVault = this.OneBtcBalanceVault;
+    this.OneBtcBalance = await this.OneBtc.balanceOf(this.issueRequester);
+    this.OneBtcBalanceVault = await this.OneBtc.balanceOf(this.vaultId);
+    assert.equal(this.OneBtcBalance.toString(), (Number(beforeOneBtcBalance)+IssueEvent.args.amount/4).toString());
+    assert.equal(this.OneBtcBalanceVault.toString(), (Number(beforeOneBtcBalanceVault) + IssueEvent.args.fee/4).toString());
+  });
 });
