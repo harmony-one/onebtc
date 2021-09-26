69057d65a8b5a60354e4bee4c29b4efd34844cad
rachit2501 <rac.sri25@gmail.com>
2021-09-27 03:07:13 +0530

    sla wrapper

--- contract/bridge/contracts/test/SlaWrapper.sol
+++ contract/bridge/contracts/test/SlaWrapper.sol
@@ -3,5 +3,12 @@ pragma solidity ^0.6.12;
 import {SLA} from "../SLA.sol";
 
 contract SLAWrapper is SLA {
-    
+    function slashVaultTest (address account) public returns (uint256){
+        uint256 slashAmount = SlashVault(account);
+        return slashAmount;
+    }
+
+    function updateSlaTes (address account, int256 delta) internal {
+        updateSLA(account, delta);
+    }
 }
