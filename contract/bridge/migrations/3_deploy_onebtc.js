1f741199b3db884355ca980a81839eeb6f520a02
yuriy <yuriy.menkov@latoken.com>
2021-12-02 18:33:18 +0300

    Validation fix for tx with many outputs

--- contract/bridge/migrations/3_deploy_onebtc.js
+++ contract/bridge/migrations/3_deploy_onebtc.js
@@ -1,15 +1,9 @@
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 const OneBtc = artifacts.require("OneBtc");
-// const RelayMock = artifacts.require("RelayMock");
-// const ExchangeRateOracleWrapper = artifacts.require("ExchangeRateOracleWrapper");
+const RelayMock = artifacts.require("RelayMock");
 
 module.exports = async function(deployer) {
-  // const IRelay = await RelayMock.deployed();
-  // const IExchangeRateOracleWrapper = await ExchangeRateOracleWrapper.deployed();
-
-  const relayAddress = "0xD38BEc54fA5067890d6B789105C428A1Bc243d42";
-  const oracleAddress = "0xEb1dF0baf6a29B5d246Cbf7072e0AA44F266555f";
-
-  const c = await deployProxy(OneBtc, [relayAddress, oracleAddress], { deployer } );
-  console.log(c.address)
+    const IRelay = await RelayMock.deployed();
+    const c = await deployProxy(OneBtc, [IRelay.address], { deployer } );
+    console.log(c.address)
 };
--- contract/bridge/scripts/deploy_onebtc_proxy.js
+++ contract/bridge/scripts/deploy_onebtc_proxy.js
@@ -1,31 +1,25 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const relayAddress = "0x695C5c8159b7d95540f725ca6119d834854F555F";
-  const oracleAddress = "0xa43A1B4643B28e11be995a20873495b71F9d5bF7";
-
   const Relay = await ethers.getContractFactory("RelayMock");
-  const relay = await Relay.attach(relayAddress);
-
-  const ExchangeRateOracle = await ethers.getContractFactory(
-    "ExchangeRateOracle"
+  const relay = await Relay.attach(
+      process.env.HMY_RELAY_CONTRACT
   );
-  const oracle = await ExchangeRateOracle.attach(oracleAddress);
+
+  const ExchangeRateOracle = await ethers.getContractFactory("ExchangeRateOracle");
+  const oracle = await ExchangeRateOracle.attach(
+      process.env.EXCHANGE_RATE_ORACLE
+  );
 
   const OneBtc = await ethers.getContractFactory("OneBtc");
-  const oneBtc = await upgrades.deployProxy(
-    // "0x31D981ADb8598CD6664Fb20e7091DEfAa8474601",
-    OneBtc,
-    [relay.address, oracle.address],
-    { initializer: "initialize" }
-  );
+  const oneBtc = await upgrades.deployProxy(OneBtc, [relay.address, oracle.address], { initializer: "initialize" });
 
   console.log("OneBtc deployed to:", oneBtc.address);
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
