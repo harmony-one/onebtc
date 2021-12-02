815cfb96ca34dbf179f7358e78ce480d1287b663
yuriy <yuriy.menkov@latoken.com>
2021-12-02 18:35:21 +0300

    Validation fix for tx with many outputs

--- contract/bridge/migrations/3_deploy_onebtc.js
+++ contract/bridge/migrations/3_deploy_onebtc.js
@@ -1,9 +1,12 @@
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 const OneBtc = artifacts.require("OneBtc");
 const RelayMock = artifacts.require("RelayMock");
+const ExchangeRateOracleWrapper = artifacts.require("ExchangeRateOracleWrapper");
 
 module.exports = async function(deployer) {
     const IRelay = await RelayMock.deployed();
-    const c = await deployProxy(OneBtc, [IRelay.address], { deployer } );
+    const IExchangeRateOracleWrapper = await ExchangeRateOracleWrapper.deployed();
+
+    const c = await deployProxy(OneBtc, [IRelay.address, IExchangeRateOracleWrapper.address], { deployer } );
     console.log(c.address)
 };
