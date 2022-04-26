edd06ecc3abff7a702b63debeb1ca278e91acd4b
apptreeso <superwebdev19@gmail.com>
2022-04-27 01:46:42 +1000

    update migration script with the external linked library

--- contract/bridge/migrations/3_deploy_onebtc.js
+++ contract/bridge/migrations/3_deploy_onebtc.js
@@ -2,11 +2,16 @@ const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 const OneBtc = artifacts.require("OneBtc");
 const RelayMock = artifacts.require("RelayMock");
 const ExchangeRateOracleWrapper = artifacts.require("ExchangeRateOracleWrapper");
+const VaultRegistryLib = artifacts.require("VaultRegistryLib");
 
 module.exports = async function(deployer) {
     const IRelay = await RelayMock.deployed();
     const IExchangeRateOracleWrapper = await ExchangeRateOracleWrapper.deployed();
+    
+    await deployer.deploy(VaultRegistryLib);
+    await VaultRegistryLib.deployed();
+    await deployer.link(VaultRegistryLib, OneBtc);
 
-    const c = await deployProxy(OneBtc, [IRelay.address, IExchangeRateOracleWrapper.address], { deployer } );
+    const c = await deployProxy(OneBtc, [IRelay.address, IExchangeRateOracleWrapper.address], { unsafeAllowLinkedLibraries: true, from: deployer } );
     console.log('OneBtc deployed at ', c.address)
 };
